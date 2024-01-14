e2a2c30791d4d08782f06614874b231c20dc1054
NoamMoskowitz <noammz101@gmail.com>
2024-01-14 12:36:56 +0200

    base for json obj retreivel

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,46 @@
+const navBtns = document.querySelectorAll(`.my-nav-link`);
 const projectLinks = document.querySelectorAll(`.btnEvent`);
-
+const porfolio = document.getElementById(`portfolio-container`);
+const projectPreview = document.getElementById(`projectPreview`);
 
 projectLinks.forEach((link) => {
-    link.addEventListener('click', (event) => {
-       sendToURL(event.target.title,event.target.href);
+    link.addEventListener('click',async (event) => {
+porfolio.style.display=`none`;
+projectPreview.style.display=`block`;
+projectPreview.style.display=`flex`
+const project =await getData();
+switch(event.target.title){
+case `htmlP1`:
+displayPreview(project.htmlP1)
+
+
+}
     });
 });
 
-function sendToURL(data,href) {
-    const dataToSend =encodeURIComponent(data);
-    const newURL = `${href}?title=${dataToSend}`;
-    window.location.href=newURL
+navBtns.forEach((navBtn)=>{
+    navBtn.addEventListener(`click`,()=>{
+        porfolio.style.display=`block`;
+projectPreview.style.display=`none`;
+
+    })
+})
+
+function displayPreview(obj) {
+    document.getElementById(`previewImage`).src = `../images/${obj.image}`
+    document.getElementById(`content-heading`).textContent = obj.title;
+    document.getElementById(`paragraph`).textContent = obj.paragraph;
+    document.getElementById(`download`).href = `../Downloads/zips/${obj.downloadLink}`
+    document.getElementById(`view`).href = ``
+
+}
+
+async function  getData() {
+    const response = await fetch(`../JSON files/projectObj.json`)
+    if ( response.ok) {
+        const responseText =await response.json();
+        return responseText
+    }else{
+        console.log(response.status);
+    }
 }
--- scripts/projectPreview.js
+++ scripts/projectPreview.js
@@ -1,13 +1,6 @@
 
 
-function displayPreview(obj) {
-    document.getElementById(`previewImage`).src = `../images/${obj.image}`
-    document.getElementById(`content-heading`).textContent = obj.title;
-    document.getElementById(`paragraph`).textContent = obj.paragraph;
-    document.getElementById(`download`).href = `../Downloads/zips/${obj.downloadLink}`
-    document.getElementById(`view`).href = ``
 
-}
 
 function getURLMessage() {
   const urlParams = new URLSearchParams(window.location.search);
@@ -24,11 +17,6 @@ function getURLMessage() {
 // Call the function and store the result in a variable
 const titleFromURL = getURLMessage();
 
-async function  getJsonObj(objName) {
-    const response = fetch(`../JSON files/projectObj.json`)
-    if (response.ok) {
-        let responseJson= (await response).json 
-    }
-}
+
 
 let json=getJsonObj()
