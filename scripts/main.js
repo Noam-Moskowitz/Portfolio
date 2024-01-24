5c300bafc9e9d0d4b0f4e1fafaec7e0d981e6276
Noam Moskowitz <noammz101@gmail.com>
2024-01-24 15:22:23 +0200

    updted main.js

--- scripts/main.js
+++ scripts/main.js
@@ -54,10 +54,10 @@ navBtns.forEach((navBtn) => {
 })
 
 function displayPreview(obj) {
-    document.getElementById(`previewImage`).src = `https://noam-moskowitz.github.io/Portfolio/images/${obj.image}`
+    document.getElementById(`previewImage`).src = `./images/${obj.image}`
     document.getElementById(`content-heading`).textContent = obj.title;
     document.getElementById(`paragraph`).textContent = obj.paragraph;
-    document.getElementById(`download`).href = `../Downloads/zips/${obj.downloadLink}`
+    document.getElementById(`download`).href = `./Downloads/zips/${obj.downloadLink}`
     document.getElementById(`view`).href = obj.viewLink;
 
 }
